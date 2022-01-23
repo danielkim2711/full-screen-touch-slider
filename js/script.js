48d6c596fa263cda6c0c3d98e7fa6d301554c961
Daniel <danielkim2711@gmail.com>
2022-01-23 20:25:19 +1300

    Remove default image dragging behaviour

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ let currentTranslate = 0;
 let previousTranslate = 0;
 let animationID = 0;
 let currentIndex = 0;
+
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img');
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+});
