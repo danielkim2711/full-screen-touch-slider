993e928925009094026d2a3b1e89384d7022e5f2
Daniel <danielkim2711@gmail.com>
2022-01-23 20:38:40 +1300

    Add event listener for touch and mouse events

--- js/script.js
+++ js/script.js
@@ -10,5 +10,34 @@ let currentIndex = 0;
 
 slides.forEach((slide, index) => {
   const slideImage = slide.querySelector('img');
-  slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+  slideImage.addEventListener('dragstart', (event) => event.preventDefault());
+
+  // Touch events
+  slide.addEventListener('touchstart', startTouch(index));
+  slide.addEventListener('touchend', endTouch);
+  slide.addEventListener('touchmove', moveTouch);
+
+  // Mouse events
+  slide.addEventListener('mousedown', startTouch(index));
+  slide.addEventListener('mouseup', endTouch);
+  slide.addEventListener('mouseleave', endTouch);
+  slide.addEventListener('mousemove', moveTouch);
 });
+
+function startTouch(index) {
+  return function (event) {
+    console.log('start');
+    dragging = true;
+  };
+}
+
+function endTouch() {
+  console.log('end');
+  dragging = false;
+}
+
+function moveTouch() {
+  if (dragging) {
+    console.log('move');
+  }
+}
